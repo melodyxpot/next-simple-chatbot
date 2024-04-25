5a63bb4a53a59e824c173da028fd4912d63ed3d6
melodyxpot <melodyxpot@gmail.com>
2024-04-25 04:52:17 -0400

    :hammer: complete all actions

--- lib/chat/actions.tsx
+++ lib/chat/actions.tsx
@@ -2,97 +2,20 @@ import 'server-only'
 
 import {
   createAI,
-  createStreamableUI,
   getMutableAIState,
   render,
   createStreamableValue
 } from 'ai/rsc'
 import OpenAI from 'openai'
 
-import { spinner, BotMessage, SystemMessage } from '@/components/stocks'
+import { BotMessage } from '@/components/stocks'
 
-import {
-  formatNumber,
-  runAsyncFnWithoutBlocking,
-  sleep,
-  nanoid
-} from '@/lib/utils'
-import { SpinnerMessage, UserMessage } from '@/components/stocks/message'
-import { Chat } from '@/lib/types'
+import { nanoid } from '@/lib/utils'
+import { SpinnerMessage } from '@/components/stocks/message'
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY || ''
 })
-
-async function confirmPurchase(symbol: string, price: number, amount: number) {
-  'use server'
-
-  const aiState = getMutableAIState<typeof AI>()
-
-  const purchasing = createStreamableUI(
-    <div className="inline-flex items-start gap-1 md:items-center">
-      {spinner}
-      <p className="mb-2">
-        Purchasing {amount} ${symbol}...
-      </p>
-    </div>
-  )
-
-  const systemMessage = createStreamableUI(null)
-
-  runAsyncFnWithoutBlocking(async () => {
-    await sleep(1000)
-
-    purchasing.update(
-      <div className="inline-flex items-start gap-1 md:items-center">
-        {spinner}
-        <p className="mb-2">
-          Purchasing {amount} ${symbol}... working on it...
-        </p>
-      </div>
-    )
-
-    await sleep(1000)
-
-    purchasing.done(
-      <div>
-        <p className="mb-2">
-          You have successfully purchased {amount} ${symbol}. Total cost:{' '}
-          {formatNumber(amount * price)}
-        </p>
-      </div>
-    )
-
-    systemMessage.done(
-      <SystemMessage>
-        You have purchased {amount} shares of {symbol} at ${price}. Total cost ={' '}
-        {formatNumber(amount * price)}.
-      </SystemMessage>
-    )
-
-    aiState.done({
-      ...aiState.get(),
-      messages: [
-        ...aiState.get().messages.slice(0, -1),
-        {
-          id: nanoid(),
-          role: 'system',
-          content: `[User has purchased ${amount} shares of ${symbol} at ${price}. Total cost = ${
-            amount * price
-          }]`
-        }
-      ]
-    })
-  })
-
-  return {
-    purchasingUI: purchasing.value,
-    newMessage: {
-      id: nanoid(),
-      display: systemMessage.value
-    }
-  }
-}
 
 async function submitUserMessage(content: string) {
   'use server'
@@ -184,42 +107,8 @@ export type UIState = {
 
 export const AI = createAI<AIState, UIState>({
   actions: {
-    submitUserMessage,
-    confirmPurchase
+    submitUserMessage
   },
   initialUIState: [],
   initialAIState: { chatId: nanoid(), messages: [] }
 })
-
-export const getUIStateFromAIState = (aiState: Chat) => {
-  return aiState.messages
-    .filter(message => message.role !== 'system')
-    .map((message, index) => ({
-      id: `${aiState.chatId}-${index}`,
-      display:
-        // message.role === 'function' ? (
-        //   message.name === 'listStocks' ? (
-        //     <BotCard>
-        //       <Stocks props={JSON.parse(message.content)} />
-        //     </BotCard>
-        //   ) : message.name === 'showStockPrice' ? (
-        //     <BotCard>
-        //       <Stock props={JSON.parse(message.content)} />
-        //     </BotCard>
-        //   ) : message.name === 'showStockPurchase' ? (
-        //     <BotCard>
-        //       <Purchase props={JSON.parse(message.content)} />
-        //     </BotCard>
-        //   ) : message.name === 'getEvents' ? (
-        //     <BotCard>
-        //       <Events props={JSON.parse(message.content)} />
-        //     </BotCard>
-        //   ) : null
-        // ) :
-        message.role === 'user' ? (
-          <UserMessage>{message.content}</UserMessage>
-        ) : (
-          <BotMessage content={message.content} />
-        )
-    }))
-}
